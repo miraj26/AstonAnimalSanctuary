ba28144f3108c99c5495332d7419b6c7ae231051
Miraj <miraj.shah@hotmail.co.uk>
2019-03-30 11:36:22 +0000

    Saving to database
    need to clean up

--- connectdb.php
+++ connectdb.php
@@ -1,9 +1,10 @@
 <?php
 	try{
-		$db = new PDO("mysql:dbname=animalSanctuary;host=localhost","miraj","123");
+		echo "test";
+		$db = new PDO("mysql:dbname=animalSanctuary;host=127.0.0.1:8889","miraj","123");
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch (PDOException $ex){
-		echo "Database error occurred. Please try again" . <br/> . $ex->getMessage();
+		echo "Database error occurred. Please try again" . "<br/>" . $ex->getMessage();
 		exit;
 	}
 ?>
--- register.php
+++ register.php
@@ -1,32 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Register</title>
-	<link rel="stylesheet" type="text/css" href="css/header.css"/>
-	<link rel="stylesheet" type="text/css" href="css/form.css"/>
-	<!--<script src="scripts/validate_password.js"></script>
-	<script src="scripts/main.js"></script>-->
-</head>
-<body>
-	<div id="container">
-		<a href="index.html">Aston Animal Santuary</a>
-	</div>
-	<div id="main">
-		<form id="register" method="post" action="register.php">
-			<input type="text" name="username" placeholder="Username" required/>	
-			<input type="Password" name="password" placeholder="Password" required/>
-			<input type="Password" name="confirm_pass" placeholder="Confirm Password" required/>
-			<input type="text" name="email" placeholder="Email" required/>
-			<input type="text" name="first" placeholder="First Name" required/>
-			<input type="text" name="second" placeholder="Last Name" required/>
-			<input type="text" name="address" placeholder="Address Line 1" required/>
-			<input type="text" name="postcode" placeholder="Postcode" required/>
-			<input type="text" name="number" placeholder="Contact Number" required/>
-			<input type="hidden" name="submitted" value="true"/>
-			<input type="submit" name="Submit" required/>
-		</form>
-	</div>
-
 	<?php
 		require_once('connectdb.php');
 
@@ -40,8 +11,10 @@
 		$contact = $_POST['number'];
 
 		try{
-			$temp = $db->prepare("INSERT INTO animalSanctuary('Username', 'Password', 'FirstName', 'LastName', 'Email', 'Address', 'Postcode', 'ContactNumber', 'Staff')" . "VALUES(?,?,?,?,?,?,?,?,?)");
-			$temp->execute($username, $password, $fname, $lname, $email, $address, $postcode, $contact, false);
+			echo "$_POST[username]";
+			$temp = $db->prepare("INSERT INTO Users(Username, Password, FirstName, LastName, Email, Address, Postcode, ContactNumber, Staff) VALUES(?,?,?,?,?,?,?,?,?)");
+			$temp->execute([$username, $password, $fname, $lname, $email, $address, $postcode, $contact, 0]);
+
 			echo("Completed");
 		} catch (PDOException $ex){
 			echo("failed to save to db");
@@ -49,5 +22,3 @@
 			exit();
 		}
 	?>
-</body>
-</html>
