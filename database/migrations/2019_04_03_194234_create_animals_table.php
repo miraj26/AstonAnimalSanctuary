59c4da788f0cb940fc50ff9384540b2567a62fb0
Miraj <miraj.shah@hotmail.co.uk>
2019-04-05 15:34:38 +0100

    laravel

---
+++ database/migrations/2019_04_03_194234_create_animals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnimalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->date('dob');
+            $table->string('description');
+            $table->binary('picture');
+            $table->boolean('availability');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, intial-scale=1">
+	<title>{{config('app.name')}}</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('/css/header.css') }}"/>
+	<link rel="stylesheet" type="text/css" href="{{ asset('/css/pagestyle.css') }}"/>
+</head>
+<body>
+	<div id="container">
+		Aston Animal Santuary
+		<!--Add Logo here-->
+		<a href="login.html" id="signin">Login</a>
+		<a href="register.html" id="signin">Register</a>
+	</div>
+	<div id="main">
+		@yield('content')
+	<div class="slide fade">
+		<img src="{{ asset('images/dogslide.jpeg')}}">
+		<div class="text" id="first">Adopt Now!</div>
+	</div>
+	<div class="slide fade">
+		<img src="{{ asset('images/catslide.jpg')}}">
+		<div class="text" id="second">Login to see all the pets you can adopt</div>
+	</div>
+	<div class="slide fade">
+		<img src="{{ asset('images/guineaslide.jpg')}}">
+		<div class="text" id="third">Change a pets life today!</div>
+	</div>
+</div>
+<div id="about">
+	<b>About Us</b>
+	<br/>
+	<section>
+			Established in 2019, Aston Pet Sanctuary has rapidly become the UK's most popular Pet Adoption website
+	</section>
+</div>
+<script src="{{ asset('js/homescript.js')}}"></script>
+
+</body>
+</html>
